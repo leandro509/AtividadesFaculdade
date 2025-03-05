6308a5cbd7abf9e04801d27d0cb24f6fce744688
Leandro Gabriel de Oliveira Nascimento <lgonascimento@furb.br>
2025-03-05 19:45:49 -0300

    correcao lista 02

--- Lista02/src/main/java/Questao02/ContaBancaria.java
+++ Lista02/src/main/java/Questao02/ContaBancaria.java
@@ -9,70 +9,80 @@ package Questao02;
  * @author leandro
  */
 public class ContaBancaria {
+
     private String numero;
     private String titular;
     private double saldo;
-    private double contaDestino;
     
+
     public String getNumero() {
-        
+
         return numero;
-        
+
     }
-    
+
     public void setNumero(String numero) {
-    
+
         this.numero = numero;
-        
+
+        if (numero.trim().isEmpty()) {
+
+            throw new IllegalArgumentException("O numero da conta esta inválido!");
+
+        }
+
     }
-    
+
     public String getTitular() {
-    
+
         return this.titular;
-        
+
     }
-    
+
     public void setTitular(String titular) {
-    
+
         this.titular = titular;
-        
+
+        if (titular.trim().isEmpty()) {
+
+            throw new IllegalArgumentException("Titular da conta é inválido!");
+
+        }
+
     }
-    
+
     public double getSaldo() {
-    
+
         return saldo;
-        
+
     }
-    
+
     public void depositar(double valor) {
-    
-        if(valor <= 0 ) {
-            
+
+        if (valor <= 0) {
+
             throw new IllegalArgumentException("Valor para de deposito invalido!");
-            
+
         }
-        
+
         this.saldo += valor;
-        
-        
+
     }
-    
-    
+
     public void sacar(double valor) {
-    
-        if((this.saldo - valor) <= 0) {
-        
-            throw new IllegalArgumentException("O valor de saque nao pode ser 0 ou negativo!");
-            
+
+        if ((this.saldo - valor) <= 0 || valor > this.saldo) {
+
+            throw new IllegalArgumentException("O valor de saque nao pode ser 0 ou saldo insuficiente!");
+
         }
-        
+
         this.saldo -= valor;
-    
+
     }
-    
-    public void transferir(ContaBancaria contaDestino, double valor ) {
-    
-        
-    
+
+    public void transferir(ContaBancaria contaDestino, double valor) {
+        sacar(valor);
+        contaDestino.depositar(valor);
     }
 }
