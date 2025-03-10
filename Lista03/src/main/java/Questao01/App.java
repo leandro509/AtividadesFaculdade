c7cffa25ad18ab775e8c3dc9beea5c185b8303a9
leandro509 <leandrogabriel509@gmail.com>
2025-03-10 16:09:45 -0300

    questao01

---
+++ Lista03/src/main/java/Questao01/App.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Questao01;
+
+/**
+ *
+ * @author leandro
+ */
+public class App {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        
+        funcionario.setNome("Leandro");
+        funcionario.setSalario(3000);
+        
+        
+        System.out.println("Funcionario: " + funcionario.getNome());
+        System.out.println("Salario: " + funcionario.getSalario());
+        System.out.println("Impostos a pagar: " + funcionario.calcularIrpf());
+    }
+}
