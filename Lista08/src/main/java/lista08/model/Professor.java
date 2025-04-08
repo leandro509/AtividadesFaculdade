94b3c091f857497d5992842c34cae51000962abb
leandro509 <leandrogabriel509@gmail.com>
2025-04-08 16:07:31 -0300

    atualizacao lista 08

---
+++ Lista08/src/main/java/lista08/model/Professor.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista08.model;
+
+/**
+ *
+ * @author leandro
+ */
+public class Professor {
+    private String nome;
+    private String email;
+    private Titulacao titulacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Titulacao getTitulacao() {
+        return titulacao;
+    }
+
+    public void setTitulacao(Titulacao titulacao) {
+        this.titulacao = titulacao;
+    }
+    
+    
+    
+}
