0f7cf94a014b12e77c29c3f4e1898207884d4e0a
Leandro Gabriel de Oliveira Nascimento <lgonascimento@furb.br>
2025-06-04 20:29:40 -0300

    exemplo HashMap

---
+++ ExemploHashMap/src/main/java/poo/exemplohashmap/Aluno.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo.exemplohashmap;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author lgonascimento
+ */
+public class Aluno {
+
+    private int idade;
+    private String nome;
+    private int matricula;
+
+    public Aluno(int idade, String nome, int matricula) {
+        setIdade(idade);
+        setNome(nome);
+        setMatricula(matricula);
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome : " + nome + " Idade : " + idade + 
+                " Matricula : " + matricula;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 11 * hash + this.idade;
+        hash = 11 * hash + Objects.hashCode(this.nome);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Aluno other = (Aluno) obj;
+        return this.matricula == other.matricula;
+    }
+    
+    
+    
+}
---
+++ ExemploHashMap/src/main/java/poo/exemplohashmap/ExemploHashMap.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.exemplohashmap;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ *
+ * @author lgonascimento
+ */
+public class ExemploHashMap {
+
+    public static void main(String[] args) {
+        HashMap<Integer, Aluno> alunos = new LinkedHashMap(); 
+        Aluno a1 = new Aluno(12 , "Gilmar", 1234);
+        alunos.put(1234, a1);
+        alunos.put(3322, new Aluno(13, "Gumercindo", 3322));
+        alunos.put(3526, new Aluno(13, "Jurandir", 3526));
+        
+        int consulta = 3322;
+        System.out.println(alunos.containsKey(consulta));
+        System.out.println("Consultando chave: " + consulta);
+        System.out.println(alunos.get(consulta));
+        
+        HashMap<String, Integer> vegetais = new LinkedHashMap();
+        
+        vegetais.put("maca", 15);
+        vegetais.put("banana", 3);
+        vegetais.put("mamao", 5);
+        vegetais.put("pera", 8);
+        vegetais.put("bergamota", 12);
+        
+        vegetais.remove("maca");
+        System.out.println("Buscando maca");
+        System.out.println(vegetais.get("maca"));
+        
+    }
+}
